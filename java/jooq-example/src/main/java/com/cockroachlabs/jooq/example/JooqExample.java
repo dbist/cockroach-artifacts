5ae1cc7fdb980b09dd24fda7fd078a71022a3444
Artem Ervits <artem@crlMBP-HQ0J2W33NQMjUx.local>
2023-11-08 16:57:18 -0500

    jOOQ example

---
+++ java/jooq-example/src/main/java/com/cockroachlabs/jooq/example/JooqExample.java
@@ -0,0 +1,45 @@
+package com.cockroachlabs.jooq.example;
+
+// For convenience, always static import your generated tables and jOOQ functions to decrease verbosity:
+import static org.jooq.impl.DSL.*;
+import test.generated.tables.Author;
+import test.generated.tables.records.AuthorRecord;
+
+
+import org.jooq.Record;
+import java.sql.*;
+import org.jooq.*;
+import org.jooq.impl.*;
+import static test.generated.tables.Author.AUTHOR;
+
+public class JooqExample {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        String userName = "root";
+        String password = "";
+        String url = "jdbc:postgresql://localhost:26257/library";
+
+        // Connection is the only JDBC resource that we need
+        // PreparedStatement and ResultSet are handled by jOOQ, internally
+        try (Connection conn = DriverManager.getConnection(url, userName, password)) {
+            DSLContext create = DSL.using(conn, SQLDialect.COCKROACHDB);
+            Result<Record> result = create.select().from(AUTHOR).fetch();
+
+            result.forEach(r -> {
+                Long id = r.getValue(AUTHOR.ID);
+                String firstName = r.getValue(AUTHOR.FIRST_NAME);
+                String lastName = r.getValue(AUTHOR.LAST_NAME);
+
+                System.out.println("ID: " + id + " first name: " + firstName + " last name: " + lastName);
+            });
+        }
+
+        // For the sake of this tutorial, let's keep exception handling simple
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
